c8925dc1bd5029684fe135d583cfbc36f8947ad5
Azamat Rakhmatilla <39348697+arapov432@users.noreply.github.com>
2020-04-09 16:27:26 -0700

    Component added

---
+++ src/components/welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Welcome extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello world. This is bullshit la la la </h1>
+            </div>
+        )
+    }
+}
+
+export default Welcome
